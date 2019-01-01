a981f31176c70df1575ea692567f1e274e45d398
Luke <bilstonluke@gmail.com>
2019-01-01 22:01:24 +1100

    update

--- assets/animation.js
+++ assets/animation.js
@@ -1,28 +1,37 @@
 function infoButton() {
-    //moves the page
-    document.getElementById("page").classList.remove("two");
-    document.getElementById("page").classList.remove("three");
-    //changes buttons
+    //title
+    document.title = "Info | sleekz.github.io"
+    //page
+    document.getElementById("p1").classList.remove("hidden");
+    document.getElementById("p2").classList.add("hidden");
+    document.getElementById("p3").classList.add("hidden");
+    //buttons
     document.getElementById("info").classList.add("sel");
     document.getElementById("css").classList.remove("sel");
     document.getElementById("js").classList.remove("sel");
 }
 
 function cssButton() {
-    //moves the page
-    document.getElementById("page").classList.add("two");
-    document.getElementById("page").classList.remove("three");
-    //changes buttons
+    //title
+    document.title = "Themes | sleekz.github.io"
+    //page
+    document.getElementById("p1").classList.add("hidden");
+    document.getElementById("p2").classList.remove("hidden");
+    document.getElementById("p3").classList.add("hidden");
+    //buttons
     document.getElementById("info").classList.remove("sel");
     document.getElementById("css").classList.add("sel");
     document.getElementById("js").classList.remove("sel");
 }
 
 function jsButton() {
-    //moves the page
-    document.getElementById("page").classList.add("three");
-    document.getElementById("page").classList.remove("two");
-    //changes buttons
+    //title
+    document.title = "Plugins | sleekz.github.io"
+    //page
+    document.getElementById("p1").classList.add("hidden");
+    document.getElementById("p2").classList.add("hidden");
+    document.getElementById("p3").classList.remove("hidden");
+    //buttons
     document.getElementById("info").classList.remove("sel");
     document.getElementById("css").classList.remove("sel");
     document.getElementById("js").classList.add("sel");
